0b24dc14782d1723cb78a57fe074cc14f574d11b
Alexey-Sennikov <nesnaiker@yandex.ru>
2020-10-17 18:09:53 +0300

    csv

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: DropdownScreen(), //DropdownScreen LoginSignupScreen
+      home: DropdownScreen(Storage()), //DropdownScreen LoginSignupScreen
     );
   }
 }
