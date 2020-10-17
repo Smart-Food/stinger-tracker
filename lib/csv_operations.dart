2a54dea6024fe2494187ebac901c2c5b29749329
Pavivin <pavivin@yandex.ru>
2020-10-17 19:47:49 +0300

    AAAHHH

---
+++ lib/csv_operations.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+class Storage {
+  Future<String> get localPath async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get localFile async {
+    final path = await localPath;
+    return File('$path/list.csv');
+  }
+
+  Future<String> readData() async {
+    try {
+      final File file = await localFile;
+      String body = await file.readAsString();
+
+      return body;
+    } catch (e) {
+      return "";
+    }
+  }
+
+  Future<File> writeData(String data) async {
+    final file = await localFile;
+    return file.writeAsString("$data");
+  }
+}
+//Button
+
+/*onPressed: (){
+            List<List> inspection = [
+              [selectedUser.name, selectedUser.damage[0]]
+            ];
+            String csv = const ListToCsvConverter(textDelimiter: '|').convert(inspection);
+            //print(csv);
+            widget.storage.writeData(csv);
+            widget.storage.readData().then((contents) {
+              setState(() {
+                fileContents = contents;
+              });
+              print(contents);
+            });
+            widget.storage.localPath.then((s){print(s);});
+          },*/
+/*
+class DropdownScreen extends StatefulWidget {
+  final Storage storage;
+  DropdownScreen(this.storage) : super();
+  State createState() => DropdownScreenState();
+}
+* */
