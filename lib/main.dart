199e2978b65227357f41444a81e7de9ded132c4d
Pavivin <pavivin@yandex.ru>
2020-10-17 04:05:38 +0300

    template

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:stinger_tracker/form.dart';
 
 import 'Screens/loginsignup/loginsignup_screen.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginSignupScreen(), //DropDown
+      home: DropdownScreen(), //DropDown LoginSignupScreen
     );
   }
 }
